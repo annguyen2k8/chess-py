bf1a23c190c2cd7ad5f3ff30eb843a48027c107a
annguyen2k8 <hacker.an1901@gmail.com>
2024-12-01 22:01:57 +0700

    rename constatn.py

--- src/classes/constant.py
+++
@@ -1,4 +0,0 @@
-from pygame.locals import *
-
-BUTTON_BACK:int = 6
-BUTTON_FORWARD:int = 7
---
+++ src/classes/constants.py
@@ -0,0 +1,7 @@
+from pygame.locals import *
+# from os import path
+
+BUTTON_BACK:int = 6
+BUTTON_FORWARD:int = 7
+
+# ASSETS = path.abspath(path.join(path.dirname(__file__), './assets'))
